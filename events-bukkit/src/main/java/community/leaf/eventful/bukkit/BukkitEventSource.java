83e9375870223d847063ffb32f4e6384845b13a7
RezzedUp <RezzedUp@users.noreply.github.com>
2022-01-03 13:19:30 -0600

    Update copyright to 2022.

--- events-bukkit/src/main/java/community/leaf/eventful/bukkit/BukkitEventSource.java
+++ events-bukkit/src/main/java/community/leaf/eventful/bukkit/BukkitEventSource.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2021, RezzedUp <http://github.com/LeafCommunity/Eventful>
+ * Copyright © 2021-2022, RezzedUp <http://github.com/LeafCommunity/Eventful>
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
